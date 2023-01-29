cb71bfb10a5a664a70d3c7a7340d16a5292ad151
TatianaShulgina2022 <118382447+TatianaShulgina2022@users.noreply.github.com>
2023-01-29 22:38:11 +0400

    сборщик

---
+++ source/js/popup.js
@@ -0,0 +1,23 @@
+const popup = document.querySelector('.popup');
+const btn = document.querySelector('.btn__js');
+const closePopup = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+
+btn.addEventListener('click', () =>{
+    popup.classList.add('open');
+});
+
+closePopup.addEventListener('click', () => {
+    popup.classList.remove('open');
+});
+
+overlay.addEventListener('click', () => {
+    popup.classList.remove('open');
+});
+
+window.addEventListener("keydown", function(e) {
+    if (e.code === "Escape" && popup.classList.contains('open')) {
+        popup.classList.remove('open');
+        btn.focus();
+    }
+});
